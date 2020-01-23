8a039a44d659596bbefd474d142a10f1abc4635a
rdodkins <rupertdodkins@gmail.com>
2020-01-22 20:36:56 -0800

    Added general_system functionality. Needs verification

---
+++ simulations/general_system/general_telescope.py
@@ -0,0 +1,149 @@
+"""
+Prescription containing a full system that can be fully toggled using switches in the parent module
+
+*** This module is currently unverified ***
+
+TODO
+    * verify this script using all the modes
+    * reimplement tiptilt again?
+
+"""
+
+import numpy as np
+import proper
+
+from medis.params import ap, tp, iop, sp
+import medis.adaptive as ao
+import medis.aberrations as aber
+import medis.optics as opx
+from medis.coronagraphy import coronagraph, vortex, apodization
+
+def general_telescope(empty_lamda, grid_size, PASSVALUE):
+    """
+    #TODO pass complex datacube for photon phases
+
+    propagates instantaneous complex E-field through the optical system in loop over wavelength range
+
+    this function is called as a 'prescription' by proper
+
+    uses PyPROPER3 to generate the complex E-field at the source, then propagates it through atmosphere, then telescope, to the focal plane
+    currently: optics system "hard coded" as single aperture and lens
+    the AO simulator happens here
+    this does not include the observation of the wavefront by the detector
+    :returns spectral cube at instantaneous time
+    """
+    # print("Propagating Broadband Wavefront Through Telescope")
+
+    # Parameters-import statements won't work through the function
+    passpara = PASSVALUE['params']
+    ap.__dict__ = passpara[0].__dict__
+    tp.__dict__ = passpara[1].__dict__
+    iop.__dict__ = passpara[2].__dict__
+    sp.__dict__ = passpara[3].__dict__
+
+    # datacube = []
+    wfo = opx.Wavefronts()
+    wfo.initialize_proper()
+
+    # Defines aperture (baffle-before primary)
+    wfo.loop_over_function(proper.prop_circular_aperture, **{'radius': tp.enterance_d/2})
+    # wfo.loop_over_function(proper.prop_define_entrance)  # normalizes the intensity
+
+    # Pass through a mini-atmosphere inside the telescope baffle
+    #  The atmospheric model used here (as of 3/5/19) uses different scale heights,
+    #  wind speeds, etc to generate an atmosphere, but then flattens it all into
+    #  a single phase mask. The phase mask is a real-valued delay lengths across
+    #  the array from infinity. The delay length thus corresponds to a different
+    #  phase offset at a particular frequency.
+    # quicklook_wf(wfo.wf_array[0,0])
+
+    if tp.obscure:
+        wfo.loop_over_function(opx.add_obscurations, M2_frac=1/8, d_primary=tp.enterance_d, legs_frac=tp.legs_frac)
+
+    if tp.use_atmos:
+        # TODO is this supposed to be in the for loop over w?
+        aber.add_atmos(wfo, *(tp.f_lens, PASSVALUE['iter']))
+        # quicklook_wf(wfo.wf_array[0, 0])
+
+    # quicklook_wf(wfo.wf_array[0,0])
+    if tp.rotate_atmos:
+        wfo.loop_over_function(aber.rotate_atmos, *(PASSVALUE['iter']))
+
+    # quicklook_wf(wfo.wf_array[0,0])
+    # vortex(wfo.wf_array[0,0])
+    # quicklook_wf(wfo.wf_array[0,0])
+
+    # Both offsets and scales the companion wavefront
+    if wfo.wf_array.shape[1] > 1:
+        opx.offset_companion(wfo)
+
+    if tp.rotate_sky:
+        wfo.loop_over_function(opx.rotate_sky, *PASSVALUE['iter'])
+
+
+    ########################################
+    # Telescope Distortions to Wavefront
+    #######################################
+    # Abberations before AO
+    if tp.aber_params['CPA']:
+        aber.add_aber(wfo, tp.f_lens, tp.aber_params, PASSVALUE['iter'], lens_name='CPA')
+        # wfo.loop_over_function(proper.prop_circular_aperture, **{'radius': tp.enterance_d / 2})
+        # wfo.wf_array = aber.abs_zeros(wfo.wf_array)
+
+    #######################################
+    # AO
+    #######################################
+
+    if tp.use_ao:
+        # ao.flat_outside(wfo.wf_array)
+        if tp.open_ao:
+            WFS_map = ao.ideal_wfs(wfo)
+            # tiptilt = np.zeros((sp.grid_size,sp.grid_size))
+        else:
+            WFS_map = PASSVALUE['CPA_maps']
+            # tiptilt = PASSVALUE['tiptilt']
+
+        # if tp.include_tiptilt:
+        #     CPA_maps, PASSVALUE['tiptilt'] = ao.tiptilt(wfo, CPA_maps, tiptilt)
+
+        if tp.include_dm:
+            ao.deformable_mirror(wfo, WFS_map, PASSVALUE['theta'])
+
+        # if not tp.open_ao:
+        #     PASSVALUE['WFS_map'] = ao.closedloop_wfs(wfo, WFS_map)
+        # ao.flat_outside(wfo.wf_array)
+
+    ########################################
+    # Post-AO Telescope Distortions
+    # #######################################
+
+    # Abberations after the AO Loop
+    if tp.aber_params['NCPA']:
+        aber.add_aber(wfo, tp.f_lens, tp.aber_params, PASSVALUE['iter'], lens_name='NCPA')
+        wfo.loop_over_function(proper.prop_circular_aperture, **{'radius': tp.enterance_d / 2})
+        # TODO does this need to be here?
+        # wfo.loop_over_function(opx.add_obscurations, tp.enterance_d/4, legs=False)
+        # wfo.wf_array = aber.abs_zeros(wfo.wf_array)
+
+    # Low-order aberrations
+    if tp.use_zern_ab:
+        wfo.loop_over_function(aber.add_zern_ab)
+
+    if tp.use_apod:
+        wfo.loop_over_function(apodization, True)
+
+    # First Optic (primary mirror)
+    wfo.loop_over_function(proper.prop_lens, tp.f_lens)
+    wfo.loop_over_function(proper.prop_propagate, tp.f_lens)
+
+    ########################################
+    # Coronagraph
+    ########################################
+    if tp.use_coronagraph:
+        wfo.loop_over_function(coronagraph, *(tp.f_lens, tp.occulter_type, tp.occult_loc, tp.enterance_d))
+
+    cpx_planes, sampling = wfo.focal_plane()
+
+    print(f"Finished datacube at timestep = {PASSVALUE['iter']}")
+
+    return cpx_planes, wfo.plane_sampling
---
+++ simulations/general_system/run_general.py
@@ -0,0 +1,90 @@
+"""
+run_general
+RD
+Jan 22 2020
+
+This is the starting point to run the general system. This general system (formerly optics_propagate.py) is fully
+customisable from this script from a series of toggles.
+
+"""
+
+import numpy as np
+
+from medis.params import iop, sp, ap, tp, cdip
+from medis.utils import dprint
+# import medis.optics as opx
+# from medis.plot_tools import view_spectra, view_timeseries, quick2D, plot_planes
+import medis.medis_main as mm
+
+tp.prescription = 'general_telescope'
+tp.obscure = True
+
+#################################################################################################
+#################################################################################################
+#################################################################################################
+# Companion
+ap.companion = True
+ap.contrast = [1e-5]
+ap.companion_xy = [[15, -15]]  # units of this still confuse me
+
+tp.enterance_d = 8
+sp.numframes = 1
+
+sp.focused_sys = True
+sp.beam_ratio = 0.14  # parameter dealing with the sampling of the beam in the pupil/focal plane
+sp.grid_size = 512  # creates a nxn array of samples of the wavefront
+sp.maskd_size = 256  # will truncate grid_size to this range (avoids FFT artifacts) # set to grid_size if undesired
+
+# Toggles for Aberrations and Control
+tp.obscure = False
+tp.use_atmos = False
+tp.use_aber = False
+tp.use_ao = True
+tp.ao_act = 14
+tp.rotate_atmos = False
+tp.rotate_sky = False
+tp.f_lens = 200.0 * tp.enterance_d
+tp.open_ao = True
+tp.include_tiptilt = False
+tp.include_dm = True
+tp.use_zern_ab = False
+tp.use_apod = False
+tp.occult_loc = [0,0]
+tp.occulter_type = 'Gaussian'
+tp.use_coronagraph = False
+
+# Plotting
+sp.show_wframe = True  # Plot white light image frame
+sp.show_spectra = True  # Plot spectral cube at single timestep
+sp.spectra_cols = 3  # number of subplots per row in view_datacube
+sp.show_tseries = False  # Plot full timeseries of white light frames
+sp.tseries_cols = 5  # number of subplots per row in view_timeseries
+sp.show_planes = True
+
+# Saving
+sp.save_obs = False  # save obs_sequence (timestep, wavelength, x, y)
+sp.save_fields = True  # toggle to turn saving uniformly on/off
+sp.save_list = ['detector']  # list of locations in optics train to save
+
+tp.aber_params = {'CPA': True,
+                    'NCPA': True,
+                    'QuasiStatic': False,  # or 'Static'
+                    'Phase': True,
+                    'Amp': False,
+                    'n_surfs': 2,
+                    'OOPP': [8, 4]}  # fraction of a focal length where mirror(s) is located
+tp.aber_vals = {'a': [7.2e-17, 3e-17],
+                  'b': [0.8, 0.2],
+                  'c': [3.1, 0.5],
+                  'a_amp': [0.05, 0.01]}
+
+if __name__ == '__main__':
+    # testname = input("Please enter test name: ")
+    testname = 'general'
+    iop.update(testname)
+    iop.makedir()
+
+    # =======================================================================
+    # Run it!!!!!!!!!!!!!!!!!
+    # =======================================================================
+    cpx_sequence, sampling = mm.run_medis()
