cb5d7e390438c17f874048744d46d0e6e248c513
jessmos <68751723+jessmos@users.noreply.github.com>
2021-07-16 11:46:54 -0700

    Add files via upload

---
+++ Adaptive Otsu Pipeline.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 18 14:13:35 2021
+
+@author: jessm
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 29 23:22:48 2020
+
+@author: jessm
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.morphology import disk
+from skimage.filters import threshold_otsu, rank
+from numpy import inf
+
+
+array=np.load('np_align38uint.npy')
+savename=('thresh_a38.npy')
+array=np.load('inpaint_a38uint.npy')
+savename=('thresh_inpaint_a38.npy')
+
+#array=np.load('np_rebinned5e7uint.npy')
+#savename=('thresh_5e7.npy')
+
+radius =5#This is the local thresholding radius
+
+def crop_center(img):
+    y,x = img.shape
+    if img.shape[0]<img.shape[1]:
+        cropx=img.shape[0]
+        startx = x//2-(cropx//2)
+        return img[:,startx:startx+cropx]
+    elif img.shape[1]<img.shape[0]:
+        cropy=img.shape[1]
+        starty = y//2-(cropy//2)    
+        return img[starty:starty+cropy,:]
+    else :
+        print("it is already a cube")
+        return img
+
+cube=crop_center(array)
+    
+selem = disk(radius)
+
+local_otsu_cube = rank.otsu(cube, selem)
+
+thresh=cube <= local_otsu_cube
+#inverted_img = invert(img)
+inv_thresh=np.invert(thresh)
+print(inv_thresh.shape)
+
+plt.imshow(local_otsu_cube, origin='lower',  cmap=plt.cm.gray)
+plt.title('Local Otsu Thresholding Mask')
+plt.colorbar()
+plt.show()
+
+
+fig, axes = plt.subplots(1, 2, figsize=(8, 5), sharex=True, sharey=True)
+ax = axes.ravel()
+plt.tight_layout()
+
+ax[0].imshow(cube, origin='lower',  cmap=plt.cm.gray)
+ax[0].set_title('Original (radius=%d)' % radius)
+ax[0].axis('off')
+
+ax[1].imshow(inv_thresh, origin='lower',  cmap=plt.cm.gray)
+ax[1].set_title('Local Thresholding Inverted(radius=%d)' % radius)
+ax[1].axis('off')
+
+plt.show()
+
+#cropping in 
+
+fig, axes = plt.subplots(1, 2, figsize=(8, 5), sharex=True, sharey=True)
+ax = axes.ravel()
+plt.tight_layout()
+
+slicecube=cube[50:80,60:90]
+slicethresh=inv_thresh[50:80,60:90]
+
+ax[0].imshow(slicecube, origin='lower',  cmap=plt.cm.gray)
+ax[0].set_title("Zoomed in Original")
+ax[0].axis('off')
+
+ax[1].imshow(slicethresh, origin='lower',  cmap=plt.cm.gray)
+ax[1].set_title('Zoomed in Thresholding')
+ax[1].axis('off')
+
+plt.show()
+
+np.save(savename, inv_thresh)
